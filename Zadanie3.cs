d4e9da90927d7371ea10daa23bdabc47ed51e5ec
Xptr98 <157038098+Xptr98@users.noreply.github.com>
2024-04-06 15:48:45 +0200

    Add files via upload

---
+++ Zadanie3.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Ścieżka do pliku
+        string filePath = "Zadanie3/tekst.txt";
+
+        try
+        {
+            using (FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                byte[] buffer = new byte[fileStream.Length];
+
+                int bytesRead = fileStream.Read(buffer, 0, buffer.Length);
+
+                string fileContent = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+                Console.WriteLine("Zawartość pliku:");
+                Console.WriteLine(fileContent);
+            }
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("Plik nie został odnaleziony.");
+        }
+        catch (IOException ex)
+        {
+            Console.WriteLine("Wystąpił błąd wejścia/wyjścia: " + ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Wystąpił nieoczekiwany błąd: " + ex.Message);
+        }
+    }
+}
